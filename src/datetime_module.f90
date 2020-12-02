647bf25cfa4c8c989bcfbf3dc5803f74d447f402
jsasaki <jsasaki.ece@gmail.com>
2020-12-02 19:13:49 +0900

    Initial commit

--- src/datetime_module.f90
+++ src/datetime_module.f90
@@ -1,3 +1,43 @@
+!MIT License
+!
+!Copyright (c) 2013-2020 datetime-fortran contributors
+
+!Permission is hereby granted, free of charge, to any person obtaining a copy
+!of this software and associated documentation files (the "Software"), to deal
+!in the Software without restriction, including without limitation the rights
+!to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+!copies of the Software, and to permit persons to whom the Software is
+!furnished to do so, subject to the following conditions:
+
+!The above copyright notice and this permission notice shall be included in all
+!copies or substantial portions of the Software.
+
+!THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+!IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+!FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+!AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+!LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+!OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+!SOFTWARE.
+
+!# Contributors
+!
+!* [Alexander Gavrikov](https://github.com/alexavr)
+!* Bjoern Hendrik Fock
+!* [Izaak Beekman](https://github.com/zbeekman)
+!* Marco Galli
+!* Mark Carter
+!* [Michael Hirsch](https://github.com/scivision)
+!* [Milan Curcic](https://github.com/milancurcic)
+!* [Oleksandr Sasha Huziy](https://github.com/guziy)
+!* [Paul Leopardi](https://github.com/penguian)
+!* [Stefano Zaghi](https://github.com/szaghi)
+!* [Tom Canich](https://github.com/tcanich)
+!* [Wadud Miah](https://github.com/wadudmiah)
+
+! Modified by Jun Sasaki
+! https://github.com/jsasaki-utokyo/datetime-fortran
+
 module datetime_module
 
   use iso_fortran_env, only: real32, real64
